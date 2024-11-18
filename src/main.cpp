d777ad3e8dba49108b8647c8d3d296b6188fbf0d
mauboru <josue21henrique@gmail.com>
2024-11-18 14:20:34 -0300

    inserindo leituras

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@
 #include "../lib/WhatsApp/WhatsApp.h"
 #include "Services/phApiSender.h"
 
-// #define READY_PIN 4  // Defina o pino correto
-// volatile int contagem = 0; // Defina a variável contagem
+#define READY_PIN 4  // Defina o pino correto
+volatile int contagem = 0; // Defina a variável contagem
 
 WifiManager wifiConnect;
-// Phmetro phMetro(0x49, 20.41272, 10); // Instancie phMetro com os parâmetros corretos
+Phmetro phMetro(0x49, 20.41272, 10); // Instancie phMetro com os parâmetros corretos
 WhatsApp whatsapp;
 phApiSender phSender;
 
@@ -22,20 +22,20 @@ unsigned long lastSendTime = 0;
 const unsigned long sendInterval = 3600000; // 36000 1 hora
 
 // Declaração da função onDataReady para interrupção
-// void IRAM_ATTR onDataReady() {
-//     contagem++;
-// }
+void IRAM_ATTR onDataReady() {
+    contagem++;
+}
 
 void setup() {
     Serial.begin(9600);
     wifiConnect.connect();
 
-    // pinMode(READY_PIN, INPUT);
-    // attachInterrupt(digitalPinToInterrupt(READY_PIN), onDataReady, FALLING);
+    pinMode(READY_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(READY_PIN), onDataReady, FALLING);
 }
 
 void loop() {
-    // phMetro.collectReading();
+    phMetro.collectReading();
 
     if (WiFi.status() != WL_CONNECTED) {
         Serial.println("WiFi desconectado, tentando reconectar...");
@@ -46,8 +46,9 @@ void loop() {
             whatsapp.sendWhatsAppMessage("teste");
             lastSendTime = millis();
         }
-
-        phSender.sendPhToApi(1); // Ajuste o valor conforme necessário
+        
+        float phValue = phMetro.calculateAveragePh();
+        phSender.sendPhToApi(phValue); // Ajuste o valor conforme necessário
     }
 
     Serial.println(5);
